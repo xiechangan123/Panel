c966920e292552f9b59d2d21afff42b42b7330cd
耗子 <i@haozi.net>
2025-01-16 01:00:21 +0800

    feat: rename slogGorm to sloggorm

--- internal/bootstrap/db.go
+++ internal/bootstrap/db.go
@@ -7,7 +7,7 @@ import (
 	"github.com/glebarez/sqlite"
 	"github.com/go-gormigrate/gormigrate/v2"
 	"github.com/knadh/koanf/v2"
-	slogGorm "github.com/orandin/slog-gorm"
+	sloggorm "github.com/orandin/slog-gorm"
 	"gorm.io/gorm"
 
 	"github.com/tnb-labs/panel/internal/app"
@@ -17,7 +17,7 @@ import (
 func NewDB(conf *koanf.Koanf, log *slog.Logger) (*gorm.DB, error) {
 	// You can use any other database, like MySQL or PostgreSQL.
 	return gorm.Open(sqlite.Open(filepath.Join(app.Root, "panel/storage/app.db")), &gorm.Config{
-		Logger:                                   slogGorm.New(slogGorm.WithHandler(log.Handler())),
+		Logger:                                   sloggorm.New(sloggorm.WithHandler(log.Handler())),
 		SkipDefaultTransaction:                   true,
 		DisableForeignKeyConstraintWhenMigrating: true,
 	})
