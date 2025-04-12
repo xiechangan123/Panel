c298a7182e11113733a87dba0db1a5b5a37ae2a5
耗子 <haozi@loli.email>
2025-04-12 20:33:14 +0800

    feat: 不限制locale

--- internal/bootstrap/t.go
+++ internal/bootstrap/t.go
@@ -1,9 +1,6 @@
 package bootstrap
 
 import (
-	"fmt"
-	"slices"
-
 	"github.com/knadh/koanf/v2"
 	"github.com/leonelquinteros/gotext"
 
@@ -11,22 +8,7 @@ import (
 )
 
 func NewT(conf *koanf.Koanf) (*gotext.Locale, error) {
-	dir, err := embed.LocalesFS.ReadDir("locales")
-	if err != nil {
-		return nil, err
-	}
-	var locales []string
-	for _, d := range dir {
-		if d.IsDir() {
-			locales = append(locales, d.Name())
-		}
-	}
-
 	locale := conf.String("app.locale")
-	if !slices.Contains(locales, locale) {
-		return nil, fmt.Errorf("failed to load locale %s, available locales: %v", locale, locales)
-	}
-
 	l := gotext.NewLocaleFSWithPath(locale, embed.LocalesFS, "locales")
 	l.AddDomain("backend")
 
