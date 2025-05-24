560b80038eaa5e8b3863fc858d88eea4b84b6067
耗子 <haozi@loli.email>
2025-05-24 22:38:12 +0800

    fix: 404时标题为undefined

--- web/src/router/guard/page-title-guard.ts
+++ web/src/router/guard/page-title-guard.ts
@@ -6,7 +6,7 @@ import { useThemeStore } from '@/store'
 export function createPageTitleGuard(router: Router) {
   const themeStore = useThemeStore()
   router.afterEach((to) => {
-    const pageTitle = translateTitle(String(to.meta.title))
+    const pageTitle = typeof to.meta.title === 'string' ? translateTitle(to.meta.title) : '404'
     if (pageTitle) document.title = `${pageTitle} | ${themeStore.name}`
     else document.title = themeStore.name
   })
