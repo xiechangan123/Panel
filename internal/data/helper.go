79828f57c9c8d9037a4e688646441f2f26ba84e4
耗子 <haozi@loli.email>
2025-11-28 04:22:42 +0800

    fix: lint

--- internal/data/helper.go
+++ internal/data/helper.go
@@ -7,7 +7,7 @@ import (
 )
 
 func getDockerClient(sock string) (*client.Client, error) {
-	apiClient, err := client.New(client.WithHost(fmt.Sprintf("unix://%s", sock)), client.WithAPIVersionNegotiation())
+	apiClient, err := client.New(client.WithHost(fmt.Sprintf("unix://%s", sock)))
 	if err != nil {
 		return nil, err
 	}
