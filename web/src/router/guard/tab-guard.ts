c3d5cdee5c8da1fd54092e39b2b3061974cfb98b
耗子 <haozi@loli.email>
2026-01-24 01:24:05 +0800

    fix: 重复创建tab

--- web/src/router/guard/tab-guard.ts
+++ web/src/router/guard/tab-guard.ts
@@ -7,7 +7,7 @@ export function createTabGuard(router: Router) {
   router.afterEach((to) => {
     if (EXCLUDE_TAB.includes(to.path)) return
     const tabStore = useTabStore()
-    const { name, fullPath: path } = to
+    const { name, path } = to
     const title = String(to.meta?.title)
     tabStore.addTab({
       name: String(name),
