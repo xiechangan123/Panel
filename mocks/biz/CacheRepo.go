4c6daf50280b90dae1fad2d8a391c1fa6dfe0e02
devhaozi <115467771+devhaozi@users.noreply.github.com>
2026-01-23 13:57:38 +0000

    chore: update mocks

--- mocks/biz/CacheRepo.go
+++ mocks/biz/CacheRepo.go
@@ -273,51 +273,6 @@ func (_c *CacheRepo_UpdateEnvironments_Call) RunAndReturn(run func() error) *Cac
 	return _c
 }
 
-// UpdateRewrites provides a mock function with no fields
-func (_m *CacheRepo) UpdateRewrites() error {
-	ret := _m.Called()
-
-	if len(ret) == 0 {
-		panic("no return value specified for UpdateRewrites")
-	}
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func() error); ok {
-		r0 = rf()
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
-// CacheRepo_UpdateRewrites_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'UpdateRewrites'
-type CacheRepo_UpdateRewrites_Call struct {
-	*mock.Call
-}
-
-// UpdateRewrites is a helper method to define mock.On call
-func (_e *CacheRepo_Expecter) UpdateRewrites() *CacheRepo_UpdateRewrites_Call {
-	return &CacheRepo_UpdateRewrites_Call{Call: _e.mock.On("UpdateRewrites")}
-}
-
-func (_c *CacheRepo_UpdateRewrites_Call) Run(run func()) *CacheRepo_UpdateRewrites_Call {
-	_c.Call.Run(func(args mock.Arguments) {
-		run()
-	})
-	return _c
-}
-
-func (_c *CacheRepo_UpdateRewrites_Call) Return(_a0 error) *CacheRepo_UpdateRewrites_Call {
-	_c.Call.Return(_a0)
-	return _c
-}
-
-func (_c *CacheRepo_UpdateRewrites_Call) RunAndReturn(run func() error) *CacheRepo_UpdateRewrites_Call {
-	_c.Call.Return(run)
-	return _c
-}
-
 // UpdateTemplates provides a mock function with no fields
 func (_m *CacheRepo) UpdateTemplates() error {
 	ret := _m.Called()
