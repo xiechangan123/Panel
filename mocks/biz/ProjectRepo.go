4dd06f58121977578d0ba34761e151a16b64b188
devhaozi <115467771+devhaozi@users.noreply.github.com>
2026-01-23 17:38:07 +0000

    chore: update mocks

--- mocks/biz/ProjectRepo.go
+++ mocks/biz/ProjectRepo.go
@@ -24,6 +24,61 @@ func (_m *ProjectRepo) EXPECT() *ProjectRepo_Expecter {
 	return &ProjectRepo_Expecter{mock: &_m.Mock}
 }
 
+// Count provides a mock function with no fields
+func (_m *ProjectRepo) Count() (int64, error) {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for Count")
+	}
+
+	var r0 int64
+	var r1 error
+	if rf, ok := ret.Get(0).(func() (int64, error)); ok {
+		return rf()
+	}
+	if rf, ok := ret.Get(0).(func() int64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ProjectRepo_Count_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'Count'
+type ProjectRepo_Count_Call struct {
+	*mock.Call
+}
+
+// Count is a helper method to define mock.On call
+func (_e *ProjectRepo_Expecter) Count() *ProjectRepo_Count_Call {
+	return &ProjectRepo_Count_Call{Call: _e.mock.On("Count")}
+}
+
+func (_c *ProjectRepo_Count_Call) Run(run func()) *ProjectRepo_Count_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run()
+	})
+	return _c
+}
+
+func (_c *ProjectRepo_Count_Call) Return(_a0 int64, _a1 error) *ProjectRepo_Count_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *ProjectRepo_Count_Call) RunAndReturn(run func() (int64, error)) *ProjectRepo_Count_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // Create provides a mock function with given fields: ctx, req
 func (_m *ProjectRepo) Create(ctx context.Context, req *request.ProjectCreate) (*types.ProjectDetail, error) {
 	ret := _m.Called(ctx, req)
