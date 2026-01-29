dd7d1ed490c244a365ef365acd9c3bef382c293a
耗子 <haozi@loli.email>
2026-01-29 22:12:55 +0800

    feat: 反向代理支持更多设置

--- web/src/views/website/EditView.vue
+++ web/src/views/website/EditView.vue
@@ -411,7 +411,10 @@ const useStaleOptions = [
 const cacheMethodOptions = [
   { label: 'GET', value: 'GET' },
   { label: 'HEAD', value: 'HEAD' },
-  { label: 'POST', value: 'POST' }
+  { label: 'POST', value: 'POST' },
+  { label: 'PUT', value: 'PUT' },
+  { label: 'PATCH', value: 'PATCH' },
+  { label: 'DELETE', value: 'DELETE' }
 ]
 
 // HTTP 协议版本选项
