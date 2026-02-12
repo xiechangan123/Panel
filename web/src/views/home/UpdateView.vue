eda6d92d66a55ddb6924a2dc17edb69360036498
耗子 <haozi@loli.email>
2026-02-12 19:05:50 +0800

    feat: 优化更新页

--- web/src/views/home/UpdateView.vue
+++ web/src/views/home/UpdateView.vue
@@ -3,10 +3,7 @@ defineOptions({
   name: 'home-update'
 })
 
-import { MdPreview } from 'md-editor-v3'
-import 'md-editor-v3/lib/style.css'
 import type { MessageReactive } from 'naive-ui'
-import { NButton } from 'naive-ui'
 import { useGettext } from 'vue3-gettext'
 
 import home from '@/api/panel/home'
@@ -17,8 +14,14 @@ const { $gettext } = useGettext()
 const { data: versions } = useRequest(home.updateInfo, {
   initialData: []
 })
+
 let messageReactive: MessageReactive | null = null
 const updateLoading = ref(false)
+
+// 解析描述文本为列表项
+const parseDescription = (text: string): string[] => {
+  return text.split('\n').filter((line) => line.trim())
+}
 
 const handleUpdate = () => {
   window.$dialog.warning({
@@ -55,27 +58,44 @@ const handleUpdate = () => {
 
 <template>
   <common-page show-footer>
-    <n-timeline v-if="versions" pt-10>
-      <n-timeline-item
-        v-for="(item, index) in versions"
-        :type="Number(index) == 0 ? 'info' : 'default'"
-        :key="index"
-        :title="item.version"
-        :time="formatDateTime(item.updated_at)"
-      >
-        <MdPreview
-          v-model="item.description"
-          noMermaid
-          noKatex
-          noIconfont
-          noHighlight
-          noImgZoomIn
-        />
-      </n-timeline-item>
-      <n-button class="ml-16" type="primary" :loading="updateLoading" :disabled="updateLoading" @click="handleUpdate">
-        {{ $gettext('Update Now') }}
-      </n-button>
-    </n-timeline>
+    <n-list v-if="versions.length" hoverable>
+      <n-list-item v-for="(item, index) in versions" :key="index">
+        <n-thing>
+          <template #header>
+            <div flex items-center gap-3>
+              <span>v{{ item.version }}</span>
+              <n-tag v-if="index === 0" type="success" size="small" :bordered="false">
+                {{ $gettext('Latest') }}
+              </n-tag>
+              <n-tag size="small" :bordered="false">
+                {{ item.type }}
+              </n-tag>
+            </div>
+          </template>
+          <template #header-extra>
+            <n-button
+              v-if="index === 0"
+              type="primary"
+              :loading="updateLoading"
+              :disabled="updateLoading"
+              @click="handleUpdate"
+            >
+              {{ $gettext('Update Now') }}
+            </n-button>
+          </template>
+          <template #description>
+            <n-text depth="3">
+              {{ formatDateTime(item.updated_at) }}
+            </n-text>
+          </template>
+          <n-ol p-0>
+            <n-li v-for="(line, i) in parseDescription(item.description)" :key="i">
+              {{ line }}
+            </n-li>
+          </n-ol>
+        </n-thing>
+      </n-list-item>
+    </n-list>
     <div v-else pt-40>
       <n-result
         status="418"
