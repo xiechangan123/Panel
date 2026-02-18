701d3e96db7118f79633d1ad579a353d5d26dd89
耗子 <haozi@loli.email>
2026-02-19 04:32:37 +0800

    feat: 调整网站统计的位置

--- web/src/views/website/IndexView.vue
+++ web/src/views/website/IndexView.vue
@@ -7,10 +7,10 @@ import BulkCreateModal from '@/views/website/BulkCreateModal.vue'
 import CreateModal from '@/views/website/CreateModal.vue'
 import EditModal from '@/views/website/EditModal.vue'
 import ListView from '@/views/website/ListView.vue'
-import StatsView from '@/views/website/StatsView.vue'
 import SettingView from '@/views/website/SettingView.vue'
+import StatsView from '@/views/website/StatsView.vue'
 
-const currentTab = ref('stats')
+const currentTab = ref('all')
 
 const createModal = ref(false)
 const bulkCreateModal = ref(false)
@@ -22,15 +22,14 @@ const editId = ref(0)
   <common-page show-header show-footer>
     <template #tabbar>
       <n-tabs v-model:value="currentTab" animated>
-        <n-tab name="stats" :tab="$gettext('Stats')" />
         <n-tab name="all" :tab="$gettext('All')" />
         <n-tab name="proxy" :tab="$gettext('Reverse Proxy')" />
         <n-tab name="php" :tab="$gettext('PHP')" />
         <n-tab name="static" :tab="$gettext('Pure Static')" />
+        <n-tab name="stats" :tab="$gettext('Stats')" />
         <n-tab name="setting" :tab="$gettext('Settings')" />
       </n-tabs>
     </template>
-    <stats-view v-if="currentTab === 'stats'" />
     <list-view
       v-if="currentTab != 'setting' && currentTab != 'stats'"
       v-model:type="currentTab"
@@ -39,6 +38,7 @@ const editId = ref(0)
       v-model:edit-modal="editModal"
       v-model:edit-id="editId"
     />
+    <stats-view v-if="currentTab === 'stats'" />
     <setting-view v-if="currentTab === 'setting'" />
     <create-modal v-model:show="createModal" v-model:type="currentTab" />
     <bulk-create-modal v-model:show="bulkCreateModal" v-model:type="currentTab" />
